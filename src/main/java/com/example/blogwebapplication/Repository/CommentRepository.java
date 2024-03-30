c368503501514a3f94f37740120fb9cc83f8ac7e
yataknemogy <savel2015@icloud.com>
2024-03-30 20:26:13 +0300

    commit

--- src/main/java/com/example/blogwebapplication/Repository/CommentRepository.java
+++ src/main/java/com/example/blogwebapplication/Repository/CommentRepository.java
@@ -1,11 +1,12 @@
 package com.example.blogwebapplication.Repository;
 
+import com.example.blogwebapplication.Model.Comment;
 import com.example.blogwebapplication.Model.Post;
 import com.example.blogwebapplication.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<Post> findById(Long id);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findById(Long id);
 }
---
+++ src/main/java/com/example/blogwebapplication/Service/CommentService.java
@@ -0,0 +1,39 @@
+package com.example.blogwebapplication.Service;
+
+import com.example.blogwebapplication.Model.Comment;
+import com.example.blogwebapplication.Model.Post;
+import com.example.blogwebapplication.Repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public Comment createComment(Comment comment){
+        return commentRepository.save(comment);
+    }
+
+    public Comment findCommentById(Long id){
+        return commentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteComment(Long id){
+        commentRepository.deleteById(id);
+    }
+
+    public List<Comment> allComments(){
+        return commentRepository.findAll();
+    }
+
+    public Comment updateComment(Comment comment){
+        Comment existingComment = commentRepository.findById(comment.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Пользователь с ID: " + comment.getId() + " не найден."));
+        existingComment.setUsername(comment.getUsername());
+        existingComment.setContent(comment.getContent());
+        return commentRepository.save(existingComment);
+    }
+}
